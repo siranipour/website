48ce255cabe9167267e36d3b69438825f7379b06
siranipour <s.iranipour@gmail.com>
2024-08-02 20:47:36 +0100

    Anchor targets are _blank

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -10,9 +10,17 @@ const Contact = ({ title }: contactProp) => {
       <Title title={title} />
       <p>
         Feel free to contact me at{" "}
-        <a href="mailto:shayan@siranipour.io">shayan [a] siranipour [dot] io</a>{" "}
-        or follow me on <a href="https://github.com/siranipour">GitHub</a> or{" "}
-        <a href="http://linkedin.com/in/shayan-iranipour">LinkedIn</a>
+        <a href="mailto:shayan@siranipour.io" target="_blank">
+          shayan [a] siranipour [dot] io
+        </a>{" "}
+        or follow me on{" "}
+        <a href="https://github.com/siranipour" target="_blank">
+          GitHub
+        </a>{" "}
+        or{" "}
+        <a href="http://linkedin.com/in/shayan-iranipour" target="_blank">
+          LinkedIn
+        </a>
       </p>
     </>
   );
--- src/components/Publications/Publications.tsx
+++ src/components/Publications/Publications.tsx
@@ -14,62 +14,104 @@ const publicationEntries: Publication[] = [
       "A new generation of simultaneous fits to LHC data using deep learning",
     date: "Jan 2022",
     journal: (
-      <a href="https://link.springer.com/article/10.1007/JHEP05(2022)032">
+      <a
+        href="https://link.springer.com/article/10.1007/JHEP05(2022)032"
+        target="_blank"
+      >
         JHEP
       </a>
     ),
-    preprint: <a href="https://arxiv.org/abs/2201.07240">2201.07240</a>,
+    preprint: (
+      <a href="https://arxiv.org/abs/2201.07240" target="_blank">
+        2201.07240
+      </a>
+    ),
   },
   {
     title:
       "An open-source machine learning framework for global analyses of parton distributions",
     date: "Sep 2021",
     journal: (
-      <a href="https://link.springer.com/article/10.1140/epjc/s10052-021-09747-9">
+      <a
+        href="https://link.springer.com/article/10.1140/epjc/s10052-021-09747-9"
+        target="_blank"
+      >
         Eur.Phys.J.C
       </a>
     ),
-    preprint: <a href="https://arxiv.org/abs/2109.02671">2109.02671</a>,
+    preprint: (
+      <a href="https://arxiv.org/abs/2109.02671" target="_blank">
+        2109.02671
+      </a>
+    ),
   },
   {
     title: "The path to proton structure at 1% accuracy",
     date: "Sep 2021",
     journal: (
-      <a href="https://link.springer.com/article/10.1140/epjc/s10052-022-10328-7">
+      <a
+        href="https://link.springer.com/article/10.1140/epjc/s10052-022-10328-7"
+        target="_blank"
+      >
         Eur.Phys.J.C
       </a>
     ),
-    preprint: <a href="https://arxiv.org/abs/2109.02653">2109.02653</a>,
+    preprint: (
+      <a href="https://arxiv.org/abs/2109.02653" target="_blank">
+        2109.02653
+      </a>
+    ),
   },
   {
     title: "Parton distributions in the SMEFT from high-energy Drell-Yan tails",
     date: "Apr 2021",
     journal: (
-      <a href="https://link.springer.com/article/10.1007/JHEP07(2021)122">
+      <a
+        href="https://link.springer.com/article/10.1007/JHEP07(2021)122"
+        target="_blank"
+      >
         JHEP
       </a>
     ),
-    preprint: <a href="https://arxiv.org/abs/2104.02723">2104.02723</a>,
+    preprint: (
+      <a href="https://arxiv.org/abs/2104.02723" target="_blank">
+        2104.02723
+      </a>
+    ),
   },
   {
     title: "The Strangest Proton?",
     date: "Aug 2020",
     journal: (
-      <a href="https://link.springer.com/article/10.1140/epjc/s10052-020-08749-3">
+      <a
+        href="https://link.springer.com/article/10.1140/epjc/s10052-020-08749-3"
+        target="_blank"
+      >
         Eur.Phys.J.C
       </a>
     ),
-    preprint: <a href="https://arxiv.org/abs/2009.00014">2009.00014</a>,
+    preprint: (
+      <a href="https://arxiv.org/abs/2009.00014" target="_blank">
+        2009.00014
+      </a>
+    ),
   },
   {
     title: "Can New Physics hide inside the proton?",
     date: "May 2019",
     journal: (
-      <a href="https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.123.132001">
+      <a
+        href="https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.123.132001"
+        target="_blank"
+      >
         Phys.Rev.Lett
       </a>
     ),
-    preprint: <a href="https://arxiv.org/abs/1905.05215">1905.05215</a>,
+    preprint: (
+      <a href="https://arxiv.org/abs/1905.05215" target="_blank">
+        1905.05215
+      </a>
+    ),
   },
 ];
 
