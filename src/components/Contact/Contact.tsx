153731fad0ef7c08d9b987700c3a76bdda25cdf5
siranipour <s.iranipour@gmail.com>
2024-08-02 21:09:55 +0100

    Attempt to obfuscate email

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -4,13 +4,18 @@ interface contactProp {
   title: string;
 }
 
+const constructEmail = () => {
+  const domain = "siranipour.io";
+  return "shayan" + "@" + domain;
+};
+
 const Contact = ({ title }: contactProp) => {
   return (
     <>
       <Title title={title} />
       <p>
         Feel free to contact me at{" "}
-        <a href="mailto:shayan@siranipour.io" target="_blank">
+        <a href={`mailto:${constructEmail()}`} target="_blank">
           shayan [a] siranipour [dot] io
         </a>{" "}
         or follow me on{" "}
