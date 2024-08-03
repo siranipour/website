b9c59162e455f05d12f1cd66c7d8511583a0c093
siranipour <s.iranipour@gmail.com>
2024-08-03 23:04:10 +0100

    Serve resume as get end point

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import express from "express";
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.static("dist/"));
+
+app.get("/resume", (req, res) => {
+  const filePath = path.join(
+    __dirname,
+    "assets",
+    "resume",
+    "shayan_iranipour_cv.pdf"
+  );
+  res.sendFile(filePath);
+});
 
 const port = process.env.PORT || 8080;
 
