283557874bd873582a98d2ce45e5bbd087f2f7f8
siranipour <s.iranipour@gmail.com>
2024-08-05 20:39:42 +0100

    Renaming header to banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header";
+import Header from "./components/Banner/Banner";
 import Contact from "./components/Contact/Contact";
 import {
   Experience,
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,4 +1,4 @@
-import style from "./Header.module.css";
+import style from "./Banner.module.css";
 import Nav from "../Nav/Nav";
 
 const Header = () => {
