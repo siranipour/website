5e87620f07501e5d9381ce9b6c10637c50469481
siranipour <s.iranipour@gmail.com>
2024-08-08 19:48:37 +0100

    Making timeline description React nodes

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -6,41 +6,83 @@ interface TimelineEntry {
   subtitle: string;
   startDate: string;
   endDate: string;
-  description: string;
+  description: React.ReactNode;
 }
 
 const timelineEntries: TimelineEntry[] = [
   {
     title: "Xantium Group",
     subtitle: "Quantitative researcher",
-    startDate: "2022-01-01",
+    startDate: "2022-02-28",
     endDate: "Present",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae autem culpa iusto sapiente unde voluptas ullam ipsa blanditiis iure hic quaerat, facere enim eveniet ex, doloremque dolores numquam at deleniti reprehenderit asperiores fuga neque facilis? Facilis eveniet necessitatibus rem aut quisquam voluptate porro dolorum itaque! Earum officiis aspernatur laboriosam, fugiat itaque iusto sit rerum amet fuga nihil saepe provident quidem dolores, corrupti nemo debitis asperiores expedita, deserunt odio! Dolorem, excepturi vero quidem cumque magni, deleniti sequi blanditiis tempore sit accusantium distinctio! Tempore iusto sequi maiores asperiores blanditiis, officia tenetur illum?",
+    description: (
+      <p>
+        <a href="https://www.xantium.com/">Xantium</a>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae autem
+        culpa iusto sapiente unde voluptas ullam ipsa blanditiis iure hic
+        quaerat, facere enim eveniet ex, doloremque dolores numquam at deleniti
+        reprehenderit asperiores fuga neque facilis? Facilis eveniet
+        necessitatibus rem aut quisquam voluptate porro dolorum itaque! Earum
+        officiis aspernatur laboriosam, fugiat itaque iusto sit rerum amet fuga
+        nihil saepe provident quidem dolores, corrupti nemo debitis asperiores
+        expedita, deserunt odio! Dolorem, excepturi vero quidem cumque magni,
+        deleniti sequi blanditiis tempore sit accusantium distinctio! Tempore
+        iusto sequi maiores asperiores blanditiis, officia tenetur illum?
+      </p>
+    ),
   },
   {
     title: "PhD",
     subtitle: "Department of Applied Mathematics and Theoretical Physics",
-    startDate: "2012-01-01",
-    endDate: "2024-01-01",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur iure a culpa unde obcaecati, minima rerum, quo ullam iste numquam consequatur quis repellendus fuga repellat minus doloribus nisi magni sint ducimus sequi aperiam neque nobis praesentium ut. Nostrum iusto optio accusantium, beatae excepturi reiciendis tempora assumenda error voluptas odio! Sequi.",
+    startDate: "2018-01-01",
+    endDate: "2022-02-21",
+    description: (
+      <p>
+        <a href="https://www.damtp.cam.ac.uk/">DAMTP</a>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur iure a
+        culpa unde obcaecati, minima rerum, quo ullam iste numquam consequatur
+        quis repellendus fuga repellat minus doloribus nisi magni sint ducimus
+        sequi aperiam neque nobis praesentium ut. Nostrum iusto optio
+        accusantium, beatae excepturi reiciendis tempora assumenda error
+        voluptas odio! Sequi. If you're interested, feel free to take a look at{" "}
+        <a href="https://www.repository.cam.ac.uk/items/6d92ca76-9aac-4be5-aefc-99d49a27406f">
+          my thesis
+        </a>
+        : Precision QCD and Effective Field Theories with Machine Learning.
+      </p>
+    ),
   },
   {
     title: "Master of Mathematics",
     subtitle: "Department of Applied Mathematics and Theoretical Physics",
-    startDate: "2012-01-01",
-    endDate: "2024-01-01",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro incidunt praesentium maiores totam quam sapiente necessitatibus modi expedita pariatur provident.",
+    startDate: "2017-01-01",
+    endDate: "2018-01-01",
+    description: (
+      <p>
+        <a href="https://en.wikipedia.org/wiki/Part_III_of_the_Mathematical_Tripos">
+          part III
+        </a>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro incidunt
+        praesentium maiores totam quam sapiente necessitatibus modi expedita
+        pariatur provident.
+      </p>
+    ),
   },
   {
     title: "Bachelor of Arts",
     subtitle: "Natural Sciences",
-    startDate: "2012-01-01",
-    endDate: "2024-01-01",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro incidunt praesentium maiores totam quam sapiente necessitatibus modi expedita pariatur provident.",
+    startDate: "2014-01-01",
+    endDate: "2017-01-01",
+    description: (
+      <p>
+        <a href="https://www.undergraduate.study.cam.ac.uk/courses/natural-sciences-ba-hons-msci">
+          natural sciences
+        </a>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro incidunt
+        praesentium maiores totam quam sapiente necessitatibus modi expedita
+        pariatur provident.
+      </p>
+    ),
   },
 ];
 
