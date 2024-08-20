f20b8f6db6c6368e175b6315dea4658415565696
siranipour <s.iranipour@gmail.com>
2024-08-20 21:01:03 +0100

    Adding template of response windows

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,7 +1,33 @@
 import { Title } from "../../components/Title/Title";
+import style from "./Chat.module.css";
 
 const Chat = () => {
-  return <Title title="siGPT" />;
+  return (
+    <>
+      <Title title="siGPT" />
+      <div className={style.input}>
+        <input></input>
+        <button>Go!</button>
+      </div>
+      <div className={style.responses}>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, illum!
+        </div>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, illum!
+        </div>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, illum!
+        </div>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, illum!
+        </div>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, illum!
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Chat;
