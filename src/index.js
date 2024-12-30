c44f30bff0b7e446bb31649e759bd20321aad7ff
siranipour <s.iranipour@gmail.com>
2024-12-30 22:50:58 +0000

    Reorder wildcard route

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ const path = require("path");
 const app = express();
 
 app.use(express.static("dist/"));
-app.get('*', function(req, res) {
-  res.sendFile('index.html', {root: "dist/"});
-});
 
 app.get("/resume", (req, res) => {
   const filePath = path.join(
     __dirname,
     "assets",
     "resume",
-    "shayan_iranipour_cv.pdf"
+    "shayan_iranipour_cv.pdf",
   );
   res.sendFile(filePath);
+});
+
+//Any route not handled by the browser is handled by the react app
+//we need this if the user requests an unidentified route directly from
+//the address bar for example.
+app.get("*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/" });
 });
 
 const port = process.env.PORT || 8080;
