6b778b7498279ca29c6e9b0dd51f3d8298fdb40b
siranipour <s.iranipour@gmail.com>
2024-12-30 22:05:09 +0000

    Modify not found message

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import style from "./NotFound.module.css";
+
 const NotFound = () => {
-  return <div>404: Page Not Found :(</div>;
+  return (
+    <>
+      <div className={style.notfound_container}>
+        <div>404: Page Not Found 😔</div>
+        <div>
+          Try returning <Link to="/">home</Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
