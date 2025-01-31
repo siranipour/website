d0c812d6e0c500879f6a038abcc74ec56fcc6bcf
siranipour <s.iranipour@gmail.com>
2025-01-31 11:10:39 +0000

    Animate response windows

--- src/pages/Projects/components/Sigpt/Response.tsx
+++ src/pages/Projects/components/Sigpt/Response.tsx
@@ -1,16 +1,24 @@
 import style from "./Response.module.css";
+import { motion, AnimatePresence } from "framer-motion";
 
-const Response = ({n}) => {
+const Response = ({ n }: { n: number }) => {
   return (
-    <>
-      <div className={style["response-container"]}>
+    <div className={style["response-container"]}>
+      <AnimatePresence>
         {Array.from({ length: n }, (_, index) => (
-          <div key={index} className="box">
+          <motion.div
+            key={index}
+            layout
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.0 }}
+            transition={{ duration: 0.1, type: "spring"}}
+          >
             Div {index + 1}
-          </div>
+          </motion.div>
         ))}
-      </div>
-    </>
+      </AnimatePresence>
+    </div>
   );
 };
 
